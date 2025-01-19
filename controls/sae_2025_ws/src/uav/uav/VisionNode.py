20625a636960f30de68ac35a01f5a611018cc4f9
ethayu <ethayu1@GMAIL.COM>
2025-01-18 22:27:22 -0500

    Revert "User/kevins4202/modes (#13)" (#14)
    This reverts commit d443e3c2ec1de89bc36f79350dca6dcc2f49ad0d.

--- controls/sae_2025_ws/src/uav/uav/VisionNode.py
+++ controls/sae_2025_ws/src/uav/uav/VisionNode.py
@@ -1,8 +1,6 @@
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Image
-from typing import Type
-from rclpy.type_support import Srv, SrvRequestT, SrvResponseT
 import cv2
 import numpy as np
 from abc import ABC, abstractmethod
@@ -37,9 +35,7 @@ class VisionNode(Node, ABC):
 
         # Internal state
         self.prev_frame = None
-
-        self.curr_frame = None
-
+        self.prev_center = (0, 0)
         self.threshold_range = None
 
     def listener_callback(self, msg: Image):
@@ -52,21 +48,9 @@ class VisionNode(Node, ABC):
         """
         try:
             frame = self.convert_image_msg_to_frame(msg)
-            self.prev_frame = self.curr_frame
-            self.curr_frame = frame
+            self.process_frame(frame)
         except Exception as e:
             self.get_logger().error(f"Failed to process image: {e}")
-
-    def service_callback(self, request: SrvRequestT, response: SrvResponseT):
-        """
-        Callback for receiving image messages. Converts the image data
-        and processes the frame.
-
-        Args:
-            msg (Image): The ROS 2 Image message.
-        """
-
-        pass
 
     def convert_image_msg_to_frame(self, msg: Image) -> np.ndarray:
         """
@@ -81,15 +65,6 @@ class VisionNode(Node, ABC):
         img_data = np.frombuffer(msg.data, dtype=np.uint8)
         frame = img_data.reshape((msg.height, msg.width, 3))  # Assuming BGR8 encoding
         return frame
-
-    def initialize_service(self, custom_service: Type[Srv[SrvRequestT, SrvResponseT]], service_name: str):
-        self.service = self.create_service(
-            custom_service,
-            service_name,
-            self.service_callback
-        )
-
-        self.get_logger().info(f"{node_name} has started, subscribing to {service_name}.")
 
     @abstractmethod
     def process_frame(self, frame: np.ndarray) -> None:
