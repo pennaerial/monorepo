e28b6a10ef02c5e84a0084cad48731d5250936e6
GavinRauh <147437901+GavinRauh@users.noreply.github.com>
2025-09-16 20:11:30 -0400

    Add Gavin Rauh profile

---
+++ fun_facts/src/team-members/gavinProfile.js
@@ -0,0 +1,7 @@
+export const profile = {
+  name: "Gavin Rauh",
+  class: { year: 2028, program: "Mechanical Engineering" },
+  location: { country: "United States", state: "New York" },
+  hobbies: ["Golf", "Philosophy", "Starting Businesses"],
+  funFacts: ["Favorite football team is the Buffalo Bills."],
+};
