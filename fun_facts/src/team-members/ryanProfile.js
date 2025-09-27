f7027b1b93210c877cd41730f31788cb1bb8c3e5
Ryan Tanenholz <44756861+ryantanen@users.noreply.github.com>
2025-09-27 17:52:51 -0400

    Add ryantanen profile

---
+++ fun_facts/src/team-members/ryanProfile.js
@@ -0,0 +1,17 @@
+export const profile = {
+  name: "Ryan Tanenholz",
+  class: {
+    year: 2028,
+    program: "SEAS",
+  },
+  location: {
+    country: "United States",
+    state: "Maryland",
+  },
+  hobbies: ["Poker", "Tennis", "Cooking", "Watching the Ravens"],
+  funFacts: [
+    "I've never lost a game of catan",
+    "I've never been to the west coast",
+    "I'm not vegan",
+  ],
+};
