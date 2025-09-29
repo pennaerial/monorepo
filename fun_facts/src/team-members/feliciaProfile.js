c932d559cd34241553cc6ae82442d2fa4f3f2fca
felicia-19 <iordachifelicia19@gmail.com>
2025-09-29 12:27:48 -0400

    Felicia's First PennAir PR (Fun Facts)

---
+++ fun_facts/src/team-members/feliciaProfile.js
@@ -0,0 +1,7 @@
+export const profile = {
+  name: "Felicia Iordachi",
+  class: { year: 2029, program: "CAS (will probably switch to SEAS)" },
+  location: { country: "Moldova", city: "Chisinau", lat: 47.00556, lng: 28.8575  }, // city/region optional
+  hobbies: ["Painting", "Running"],
+  funFacts: ["I speak some german, a little bit of russian, an even smaller bit of turkish, and romanian"],
+};
