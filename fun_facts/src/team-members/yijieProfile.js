90122d87123cb6de7bb726597e75f919a3bc01c2
Yijie <yijie@ist2-00820.apn.wlan.private.upenn.edu>
2025-10-01 17:33:52 -0400

    Add profile card for Yijie Guo

---
+++ fun_facts/src/team-members/yijieProfile.js
@@ -0,0 +1,11 @@
+export const profile = {
+  name: "Yijie Guo",
+  class: { year: 2027, program: "SEAS" },
+  location: { country: "China", city: "Inner Mongolia" },
+  hobbies: ["Eating", "Exercising"],
+  funFacts: ["I'm interesting in both tech and history :)"],
+};
+
+
+
+
