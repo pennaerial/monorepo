eac957d3921c7d2998a58f412bbc00d4a3661e26
Skylar <skylar.xty@gmail.com>
2025-10-04 17:12:58 -0400

    tianyi-add

---
+++ fun_facts/src/team-members/tianyiProfile.js
@@ -0,0 +1,11 @@
+export const profile = {
+  name: "Tianyi Xia",
+  class: { year: 2027, program: "MSE-RObotics" },
+  location: { country: "China", city: "Nanjing" },
+  hobbies: ["Soccer","Hiking","Music"],
+  funFacts: ["I am a 12-year Bayern Munich fan."],
+};
+
+
+
+
