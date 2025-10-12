412a766c5c8bc6a924c981e5b108c90d4761913d
dzhan111 <zhandavid4@gmail.com>
2025-10-12 18:30:44 -0400

    ros2 package setup files

---
+++ controls/sae_2025_ws/src/sim/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+import os
+from glob import glob
+
+package_name = 'sim'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=['sim', 'sim.in_house', 'sim.in_house.courses'],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='pennair',
+    maintainer_email='pennair@example.com',
+    description='Simulation framework for UAV competitions',
+    license='MIT',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'scoring_node = sim.in_house.scoring_node:main',
+        ],
+    },
+)
