4441c80fdd2114b0669a2300279998ed5f7ce288
yuzhiliu8 <yuzhiliu8@gmail.com>
2025-11-01 17:50:12 -0400

    temu node TYPE SHIT - YL, JK

--- controls/sae_2025_ws/src/uav/uav/cv/find_hoop.py
+++ controls/sae_2025_ws/src/uav/uav/cv/find_hoop.py
@@ -22,53 +22,71 @@ def find_hoop(
     copy = cv2.cvtColor(copy, cv2.COLOR_RGB2GRAY, None)
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV, None)
 
-    lower_bound = np.array([0, 0, 0])
+    lower_bound = np.array([0, 50, 50])
     upper_bound = np.array([10, 255, 255])
 
     mask1 = cv2.inRange(hsv, lower_bound, upper_bound)
     mask2 = cv2.inRange(hsv, (0, 0, 0), (180, 80, 80))
     combined_mask = cv2.bitwise_or(mask1, mask2)
-
     blurred = cv2.blur(combined_mask, (7, 7), None)
-
     retval, thresh1 = cv2.threshold(blurred, 10, 255, cv2.THRESH_BINARY_INV)
-
     h, w = img.shape[:2]
+
+    ys, xs = np.where(mask1 == 255)
+    if len(xs) > 0:
+        avg_x = xs.mean()
+        avg_y = ys.mean()
+    else:
+        avg_x, avg_y = None, None
+    if avg_x is not None:
+        if avg_x >= w//2:
+            if avg_y <= h//2: #right top
+                seed_points = [(0, 0), (0, h-1), (w-1, h-1)]
+            else: #right bottom
+                seed_points = [(0, 0), (w-1, 0), (0, h-1)]
+        else:
+            if avg_y <= h//2: #left top
+                seed_points = [(w-1, 0), (0, h-1), (w-1, h-1)]
+            else: #left bottom
+                seed_points = [(0, 0), (w-1, 0), (w-1, h-1)]
+    else:
+        seed_points = [(0, 0), (w-1, 0), (0, h-1), (w-1, h-1)]
+
     mask = np.zeros((h + 2, w + 2), np.uint8)
-    seed_points = [(0, 0), (w-1, 0), (0, h-1), (w-1, h-1)]
     for point in seed_points:
         if thresh1[point[1]][point[0]] != 0:
             cv2.floodFill(thresh1, mask, point, 0)
-
-
     contours, hierarchy = cv2.findContours(thresh1, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    
-    selected_contour = None
-
+    if len(contours) > 0:
+        selected_contours = contours[0]
+    else: selected_contours = []
     maxArea = 0
     maxScore = 0
+
+    cv2.imshow("thresh", mask1)
     for contour in contours:
         score = 0
         area = cv2.contourArea(contour)
-
+        perimeter = cv2.arcLength(contour, True)
+        # if area > 1000:
+        #     if perimeter > 0:
+        #         circularity = (4 * math.pi * area) / (perimeter ** 2)
+        #         if circularity > 0.3:
+        #             selected_contours.append(contour)
         if area > maxArea:
             score += area
             maxArea = area
-
         moments = cv2.moments(contour) #returns a dictionary of moments
         if moments['m00'] != 0:
             Cx = int(moments['m10'] / moments['m00'])
             Cy = int(moments['m01'] / moments['m00'])
         else:
             Cx = w//2
-            Cy = h//2
-
         score -= abs(Cx - w//2)
         if score > maxScore:
-            selected_contour = contour
-
-
-    if selected_contour is not None:
+            selected_contours = [contour]
+    for contour in selected_contours:
+        # cv2.drawContours(img, contour, -1, (255, 0, 0))
         moments = cv2.moments(contour) #returns a dictionary of moments
         if moments['m00'] != 0:
             Cx = int(moments['m10'] / moments['m00'])
@@ -78,5 +96,4 @@ def find_hoop(
             cv2.arrowedLine(img, (w//2, h//2), (Cx, Cy ), (255, 0, 0), 2, cv2.LINE_AA, 0, 0.1)
             return Cx - w//2, Cy - h//2, img
         return None, None, img
-    else:
-        return None, None, img        
+    return None, None, img
--- controls/sae_2025_ws/src/uav/uav/vision_nodes/TemuVisionNode.py
+++ controls/sae_2025_ws/src/uav/uav/vision_nodes/TemuVisionNode.py
@@ -2,8 +2,9 @@
 import cv2
 import numpy as np
 from uav.cv.find_hoop import find_hoop
-from uav.vision_nodes import VisionNode
+from uav.vision_nodes.VisionNode import VisionNode
 import rclpy
+from cv_bridge import CvBridge
 from uav.utils import pink, green, blue, yellow
 
 class TemuVisionNode(VisionNode):
@@ -12,11 +13,16 @@ class TemuVisionNode(VisionNode):
     """
     def __init__(self):
         super().__init__('hoop_tracking', display=False, use_service=False)
+        self.bridge = CvBridge()
+
 
     def image_callback(self, msg):
-        image = self.convert_image_msg_to_frame(msg)
+        self.get_logger().info("Received image for hoop tracking.")
+        self.sim = True
+        image = self.bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
         x, y, img = find_hoop(image)
-        cv2.imshow("Result", img)
+        self.get_logger().info(str(type(img)))
+        self.display_frame(img, "result")
 
 
 def main():
