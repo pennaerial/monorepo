14d95033e0c8b91f8bb1a0525475413eb926ee80
0frankie <frankiel06@proton.me>
2025-11-05 03:57:03 +0000

    added hoopmode template

--- controls/sae_2025_ws/src/uav/uav/autonomous_modes/HoopMode.py
+++ controls/sae_2025_ws/src/uav/uav/autonomous_modes/HoopMode.py
@@ -7,10 +7,10 @@ from uav.vision_nodes import PayloadTrackingNode
 from typing import Optional, Tuple
 import cv2
 
-
 class HoopMode(Mode):
     """
-    A mode for identifying hoops and passing through them.
+    A mode that initiates by flying up, trying to find a hoop, and continues
+    attempting to find and look for hoops.
     """
 
     def __init__(self, node: Node, uav: UAV, color: str = 'orange'):
@@ -25,10 +25,12 @@ class HoopMode(Mode):
         super().__init__(node, uav)
 
         self.response = None
-        self.altitude_constant = 3
+        self.altitude_constant = 1
+        self.wait_time = 20_000         # delay in ms
         self.done = False
-        self.color = color
+        self.color = color          # might be useful as a way to confirm
         self.goal_pos = None
+        self.passed_hoops = []      # list of coordinates of all passed hoops
 
     def on_update(self, time_delta: float) -> None:
         """
@@ -38,49 +40,45 @@ class HoopMode(Mode):
         if self.uav.roll > 0.1 or self.uav.pitch > 0.1:
             self.log("Roll or pitch detected. Waiting for stabilization.")
             return
-          
-        request = PayloadTracking.Request()
+        
+        request = PayloadTracking.Request() # TODO: change this to new vision node
         request.altitude = -self.uav.get_local_position()[2]
         request.yaw = float(self.uav.yaw)
-        request.payload_color = self.color
         response = self.send_request(PayloadTrackingNode, request)
         
         # If no payload pose is received, exit early
         if response is None:
+            direction = [0, 0, -self.altitude_constant] # start going upwards
+            self.uav.publish_position_setpoint(direction, relative=True)
+            self.wait_time -= time_delta
+
+            if self.wait_time <= 0: # if 20 seconds has elapsed with no response
+                self.done = True
             return
+        
+        self.wait_time = 20_000
+        self.goal_pos = self.fetch_cv_coordinates()
 
         if self.goal_pos:
             self.uav.publish_position_setpoint(self.goal_pos)
             if self.uav.distance_to_waypoint('LOCAL', self.goal_pos) <= 0.05:
+                # why is there an "or True" here? what is dlz?
                 if response.dlz_empty or True:
                     self.done = True
                 else:
                     pass # TODO: Extend servo
             return
         
-        # Camera is FRONT-FACING, therefore we need to transform the x,y,z from the Vision Node to Correct Drone Coordinates
-        direction = [response.direction[2], response.direction[0],
-                        response.direction[1] / self.altitude_constant]
+        direction = [ -response.direction[1], response.direction[0], response.direction[2] ]
         
         camera_offsets = tuple(x / request.altitude for x in self.uav.camera_offsets) if request.altitude > 1 else self.uav.camera_offsets
-
         direction = [x + y for x, y in zip(direction, self.uav.uav_to_local(camera_offsets))]
-
-        # Determine the direction vector based on altitude and payload pose
-        if request.altitude < 1:
-            # If payload pose direction is within a small threshold
-            if (np.abs(direction[0]) < request.altitude / 50 and
-                np.abs(direction[1]) < request.altitude / 50):
-                if request.altitude < 0.5:
-                    self.goal_pos = self.uav.get_local_position()
-                    return
-                else:
-                    direction = [0, 0, request.altitude / self.altitude_constant]
-            else:
-                direction[2] = 0
 
         self.log(f"Direction: {direction}")
         self.uav.publish_position_setpoint(direction, relative=True)
+    
+    def fetch_cv_coordinates(self) -> tuple[float, float, float] | None:
+        pass # TODO: implement call to cv once pushed
 
     def check_status(self) -> str:
         """
