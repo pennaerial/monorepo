ba9bba20d21079290ca55a71bdda8e8a1974e5c7
yuzhiliu8 <yuzhiliu8@gmail.com>
2025-11-05 20:50:04 -0500

    directions publishing

--- controls/sae_2025_ws/src/uav/uav/vision_nodes/TemuVisionNode.py
+++ controls/sae_2025_ws/src/uav/uav/vision_nodes/TemuVisionNode.py
@@ -6,6 +6,7 @@ from uav.vision_nodes.VisionNode import VisionNode
 import rclpy
 from cv_bridge import CvBridge
 from uav.utils import pink, green, blue, yellow
+from geometry_msgs.msg import Vector3
 
 class TemuVisionNode(VisionNode):
     """
@@ -15,14 +16,23 @@ class TemuVisionNode(VisionNode):
         super().__init__('hoop_tracking', display=False, use_service=False)
         self.bridge = CvBridge()
 
+        self.hoop_directions_publisher = self.create_publisher(msg_type=Vector3, topic='/hoop_directions', qos_profile=10, )
+
 
     def image_callback(self, msg):
-        self.get_logger().info("Received image for hoop tracking.")
         self.sim = True
         image = self.bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
         x, y, img = find_hoop(image)
-        self.get_logger().info(str(type(img)))
         self.display_frame(img, "result")
+        z = 1.0
+        self.publish_directions(x, y, z)
+    
+    def publish_directions(self, x, y, z):
+        msg = Vector3()
+        msg.x = float(x)
+        msg.y = float(y)
+        msg.z = float(z)
+        self.hoop_directions_publisher.publish(msg)
 
 
 def main():
