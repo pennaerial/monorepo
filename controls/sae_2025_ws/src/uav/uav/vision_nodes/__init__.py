1ca3f7385a04f8528348c4c17a5aeb681430cbc8
Isha Bhatia <ishabhatia@hnt-13185.apn.wlan.private.upenn.edu>
2025-11-09 21:54:48 -0500

    Add Kalman Filter state estimation node

--- controls/sae_2025_ws/src/uav/uav/vision_nodes/__init__.py
+++ controls/sae_2025_ws/src/uav/uav/vision_nodes/__init__.py
@@ -1,2 +1,4 @@
 from .VisionNode import VisionNode # make sure to import the parent class FIRST (to avoid circular imports)
 from .PayloadTrackingNode import PayloadTrackingNode
+from .kalman_filter_node import StateEstimationNode
+from .pose_estimator_node import PoseEstimatorNode
