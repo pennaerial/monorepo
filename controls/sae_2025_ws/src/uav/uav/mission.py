e9d560c491ff19c2ab1f6ed335fd6d048edb7ee7
Vaughan Reale <vaughanreale@Vaughans-MacBook-Pro-2.local>
2025-11-14 16:58:37 -0500

    fix mission

--- controls/sae_2025_ws/src/uav/uav/mission.py
+++ controls/sae_2025_ws/src/uav/uav/mission.py
@@ -5,12 +5,19 @@ import os
 from uav.ModeManager import ModeManager
 
 def main():
-    if len(sys.argv) > 2:
+    if len(sys.argv) > 3:
         debug = sys.argv[1]
         yaml_file = sys.argv[2]
+        servo_only = sys.argv[3]
+        try:
+            camera_offsets = [float(offset.strip()) for offset in sys.argv[4].split(',')]
+        except Exception as e:
+            print(f"Error parsing camera_offsets: {e}")
+            camera_offsets = [0, 0, 0]
+        vision_nodes = sys.argv[5]
     else:
         cwd = os.getcwd() # default
-        yaml_file = f'{cwd}/src/sim/sim/simulations/in_house.yaml'
+        yaml_file = f'{cwd}/src/uav/uav/missions/basic.yaml'
         vision_nodes = ''
         debug = 'false'
         camera_offsets = [0, 0, 0]
