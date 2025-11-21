890b6aff499d508e552c4a9056bcd5c59cc6ba5e
Yanqian-W <yqwang12@seas.upenn.edu>
2025-11-21 00:54:25 -0500

    update vision to fit hoop_detect cv

--- controls/sae_2025_ws/src/uav/uav/vision_nodes/HoopTrackingNode.py
+++ controls/sae_2025_ws/src/uav/uav/vision_nodes/HoopTrackingNode.py
@@ -3,8 +3,7 @@ import cv2
 import numpy as np
 from uav.cv.tracking import rotate_image
 from uav.vision_nodes import VisionNode
-from uav.cv.pose_estimate import estimate_hoop_pose_from_contour
-from uav.cv.hoop_detect import find_hoop
+from uav.cv.hoop_detect import detect_contour, estimate_pose_from_contour
 import rclpy
 from uav_interfaces.srv import HoopTracking
 
@@ -12,17 +11,16 @@ class HoopTrackingNode(VisionNode):
 
     srv = HoopTracking
     
-    def __init__(self, display: bool = False):
+    def __init__(self):
         super().__init__('hoop_tracking', self.__class__.srv)
         
         cv2.startWindowThread()
         cv2.namedWindow("Bruh", cv2.WINDOW_NORMAL)
-        self.display = display
+
         self.create_service(HoopTracking, self.service_name(), self.service_callback)
         
     def service_callback(self, request, response):
         """Main loop: get camera image, detect hoop, estimate pose, and publish."""
-
         # Request image
         image_msg, camera_info = self.request_data(cam_image=True, cam_info=True)
         if image_msg is None:
@@ -32,10 +30,11 @@ class HoopTrackingNode(VisionNode):
         # frame = rotate_image(frame, -np.rad2deg(request.yaw))
 
         # Detect the largest contour
-        contour = find_hoop(frame)  # (N,1,2)
+        ellipse_contour, ellipse_params = detect_contour(frame)
         # PnP pose estimation
-        success, rvec, tvec, c_obj, ellipse = estimate_hoop_pose_from_contour(
-            contour,
+        success, rvec, tvec, c_obj, ellipse = estimate_pose_from_contour(
+            ellipse_contour, 
+            ellipse_params,
             camera_matrix = np.array(camera_info.k).reshape(3,3),
             dist_coeffs = np.array(camera_info.d)
         )
@@ -49,19 +48,13 @@ class HoopTrackingNode(VisionNode):
         #     response.ellipse = [float(cx), float(cy), float(major), float(minor), float(angle)]
         # else:
         #     response.ellipse = [0.0,0.0,0.0,0.0,0.0]
-
-        # Visualization
-        if self.display:
-            debug_img = frame.copy()
-            cv2.drawContours(debug_img, [contour.astype(int)], -1, (0, 255, 0), 2)
-            self.display_frame(debug_img, "Hoop Tracking")
     
         return response
 
 
 def main():
     rclpy.init()
-    node = HoopTrackingNode(display=False)
+    node = HoopTrackingNode()
     try:    
         rclpy.spin(node)
     except Exception as e:
