6ad5d9db38df15d53fb161fe83da9961ea1ff3b2
Michael Licamele <licamelem@gmail.com>
2026-01-31 21:50:17 +0000

    Put dlz at same location as drone spawn point for pid tuning purposes

--- controls/sae_2025_ws/src/sim/sim/world_gen/CustomWorldNode.py
+++ controls/sae_2025_ws/src/sim/sim/world_gen/CustomWorldNode.py
@@ -18,9 +18,9 @@ class CustomWorldNode(WorldNode):
     
     def generate_world(self):
         dlz = Entity(
-            name="dlz_pink_8x8",
-            path_to_sdf="~/.simulation-gazebo/models/dlz_pink_8x8/model.sdf",
-            position=(5.0, 10.0, 0.0),
+            name="dlz",
+            path_to_sdf="~/.simulation-gazebo/models/dlz/model.sdf",
+            position=(-5.0, 0.0, 0.0),
             rpy=(0.0, 0.0, 0.0),
             world="custom"
         )
