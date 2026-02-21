b5559262899a4f40c60a3b5b0e239c18d4f953b9
yuzhiliu8 <yuzhiliu8@gmail.com>
2026-02-21 17:55:59 -0500

    mode LOW

--- controls/sae_2025_ws/src/payload/src/motor.cpp
+++ controls/sae_2025_ws/src/payload/src/motor.cpp
@@ -3,8 +3,8 @@
 Motor::Motor(int handle, int phase, int enable) {
     handle_ = handle;
 
-    phase_ = std::make_unique<GPIO>(handle_, phase, static_cast<uint8_t>(PinType::Binary));
-    enable_ = std::make_unique<GPIO>(handle_, enable, static_cast<uint8_t>(PinType::PWM));
+    phase_ = std::make_unique<GPIO>(handle_, phase, PinType::Binary | PinType::PWM ); //in1
+    enable_ = std::make_unique<GPIO>(handle_, enable, PinType::Binary | PinType::PWM); //in2
 }
 
 void Motor::set_speed(float speed) {
@@ -13,11 +13,18 @@ void Motor::set_speed(float speed) {
         RCLCPP_WARN(rclcpp::get_logger("motor"), "Clamped speed from %f to %f", speed, norm_speed);
     }
 
-    if (norm_speed > 0) { //Forward, set phase to 0
-        phase_->write_low();
-    } else {
-        phase_->write_high();
-    }
     float pwm_duty_cycle = abs(norm_speed * 100.0f); //percentage
-    enable_->write_pwm(500.0f, pwm_duty_cycle, 0, 0);
+    if (norm_speed > 0) { //forward, set phase to 0
+        phase_->write_high();
+        enable_->write_pwm(500.0f, pwm_duty_cycle, 0, 0);
+    } else if (norm_speed < 0){ //reverse
+        phase_->write_pwm(500.0f, pwm_duty_cycle, 0, 0);
+        enable_->write_high();
+    } else { //brake
+        phase_->write_high();
+        enable_->write_high();
+    }
+
+
+
 }
