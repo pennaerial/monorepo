2af4cf5a60150003e551ed95eb874eb0c825588e
yuzhiliu8 <yuzhiliu8@gmail.com>
2026-07-09 23:20:33 -0400

    unit testing mode_loader

--- controls/sae_2025_ws/src/vehicle_common/vehicle_common/mode_loader.py
+++ controls/sae_2025_ws/src/vehicle_common/vehicle_common/mode_loader.py
@@ -7,29 +7,6 @@ from uav.vision_nodes import VisionNode
 import importlib
 import pkgutil
 
-_mode_registry: dict[str, RegisteredMode] = {}
-
-def serialize_type(cls: type) -> str:
-    return f"{cls.__module__}:{cls.__qualname__}"
-
-def deserialize_type(path: str, base_cls: type) -> type:
-    try:
-        module_path, class_name = path.split(":", 1)
-    except ValueError:
-        raise ValueError(f"Invalid type path '{path}'. Expected 'module:ClassName'")
-
-    module = importlib.import_module(module_path)
-    obj = module
-    for part in class_name.split("."):
-        obj = getattr(obj, part)
-
-    obj = cast(type, obj)
-    if not issubclass(obj, base_cls):
-        raise ValueError(f"{path} must be a {base_cls.__name__}")
-
-    return obj
-
-
 class RegisteredMode(BaseModel):
     model_config = ConfigDict(arbitrary_types_allowed=True)
 
@@ -55,20 +32,49 @@ class RegisteredMode(BaseModel):
 
     @field_validator("mode_cls", mode="before")
     @classmethod
-    def derserialize_mode_cls(cls, path: str):
-        return deserialize_type(path, type[Mode])
+    def deserialize_mode_cls(cls, path) -> type[Mode]:
+        if isinstance(path, str):
+            return deserialize_type(path, Mode)
+        return path # mode type
 
     @field_validator("targets", mode="before")
     @classmethod
-    def derserialize_targets(cls, paths: list[str]):
-        return [deserialize_type(p, type[Vehicle]) for p in paths]
+    def deserialize_targets(cls, paths) -> list[type[Vehicle]]:
+        if all(isinstance(p, str) for p in paths):
+            return [deserialize_type(p, Vehicle) for p in paths]
+        return paths # Vehicle types
 
-    @field_validator("targets", mode="before")
+    @field_validator("required_vision_nodes", mode="before")
     @classmethod
-    def derserialize_vision_nodes(cls, paths: list[str]):
-        return [deserialize_type(p, type[VisionNode]) for p in paths]
+    def deserialize_vision_nodes(cls, paths) -> list[type[VisionNode]]:
+        if all(isinstance(p, str) for p in paths):
+            return [deserialize_type(p, VisionNode) for p in paths]
+        return paths
 
 
+class ModeRegistry(BaseModel):
+    modes: dict[str, RegisteredMode]
+
+
+def serialize_type(cls: type) -> str:
+    return f"{cls.__module__}:{cls.__qualname__}"
+
+def deserialize_type(path: str, base_cls: type) -> type:
+    try:
+        module_path, class_name = path.split(":", 1)
+    except ValueError:
+        raise ValueError(f"Invalid type path '{path}'. Expected 'module:ClassName'")
+
+    module = importlib.import_module(module_path)
+    obj = module
+    for part in class_name.split("."):
+        obj = getattr(obj, part)
+
+    obj = cast(type, obj)
+    if not issubclass(obj, base_cls):
+        raise ValueError(f"{path} must be a {base_cls.__name__}")
+
+    return obj
 
 def register_mode(
     id: str,
@@ -79,11 +85,11 @@ def register_mode(
     transition_labels: list[str] = []
     ):
     def decorator(registered_mode_cls: type[Mode]):
-        if id in _mode_registry:
+        if id in _mode_registry.modes:
             raise ValueError(
                     f"Id {id} already has a registered mode for it"
             )
-        _mode_registry[id] = RegisteredMode(
+        _mode_registry.modes[id] = RegisteredMode(
             id=id,
             targets=targets,
             mode_cls=registered_mode_cls,
@@ -113,11 +119,13 @@ def discover_modes(modules: list[str] = ["uav.modes", "payload.modes"]):
                 print(e)
                 continue
 
-def get_mode_cls(id: str) -> type[Mode]:
+_mode_registry = ModeRegistry(modes=dict())
+
+def get_registered_mode(id: str) -> RegisteredMode:
     try:
-        return _mode_registry[id].mode_cls
+        return _mode_registry.modes[id]
     except KeyError:
-        available = " ".join(_mode_registry.keys())
+        available = " ".join(_mode_registry.modes.keys())
 
         raise KeyError(
             f"Mode '{id}' not found."
