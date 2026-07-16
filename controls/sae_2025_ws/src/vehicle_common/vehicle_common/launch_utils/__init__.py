645b4d1bfb23ee2b489ef7d8864523127cfe37ba
yuzhiliu8 <yuzhiliu8@gmail.com>
2026-07-16 19:56:58 -0400

    launch utils file. Probably need to split into cleaner sub files just in case

---
+++ controls/sae_2025_ws/src/vehicle_common/vehicle_common/launch_utils/__init__.py
@@ -0,0 +1,17 @@
+from .launch_utils import format_bullet_list, get_logger
+from .launch_utils import LaunchError
+from .launch_utils import include_launch
+from .launch_utils import check_unknown_launch_args
+from .launch_utils import on_emitted_event
+from .launch_utils import format_bullet_list
+
+
+__all__ = [
+    "get_logger",
+    "LaunchError",
+    "include_launch",
+    "check_unknown_launch_args",
+    "on_emitted_event",
+    "format_bullet_list",
+]
+
