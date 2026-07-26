55139f57092db0e96ef174a7b4151f41960555a8
yuzhiliu8 <yuzhiliu8@gmail.com>
2026-07-25 21:20:12 -0400

    dev_env for PENNAIR/monorepo global env vars

---
+++ dev_env.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+export MONOREPO_ROOT="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
+export PENNAIR_GZ_MODELS_PATH="${MONOREPO_ROOT}/gz-models"
+export PENNAIR_PX4_PATH="${MONOREPO_ROOT}/PX4-Autopilot"
