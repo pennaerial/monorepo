d63ac70b57fe3d09bb730a93714b3d1acb52ca4e
yuzhiliu8 <yuzhiliu8@gmail.com>
2026-07-25 21:19:52 -0400

    added is_truthy and prepend_env_path helpers

--- controls/sae_2025_ws/src/vehicle_common/vehicle_common/launch_utils/__init__.py
+++ controls/sae_2025_ws/src/vehicle_common/vehicle_common/launch_utils/__init__.py
@@ -3,6 +3,8 @@ from .launch_utils import LaunchError
 from .launch_utils import include_launch
 from .launch_utils import check_unknown_launch_args
 from .launch_utils import format_bullet_list
+from .launch_utils import is_truthy
+from .launch_utils import prepend_env_path
 
 
 __all__ = [
@@ -11,4 +13,6 @@ __all__ = [
     "include_launch",
     "check_unknown_launch_args",
     "format_bullet_list",
+    "is_truthy",
+    "prepend_env_path"
 ]
