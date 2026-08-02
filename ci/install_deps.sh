3ebd07cceb4fb1512cc2cfa3ea4313e3c8cc4788
yuzhiliu8 <yuzhiliu8@gmail.com>
2026-08-02 19:28:58 -0400

    add px4 autopilot dependencies install step

--- ci/install_deps.sh
+++ ci/install_deps.sh
@@ -19,10 +19,14 @@ rosdep install -r -i -y --rosdistro "$ROS_DISTRO" \
 
 #### Install all apt packages (non rosdep)
 apt-get install -y --no-install-recommends "${APT_PACKAGES[@]}"
-apt-get clean
-rm -rf /var/lib/apt/lists/*
 
 #### install python dependencies into system using uv
 echo "Installing pyproject dependencies globally..."
 uv pip install --system --break-system-packages --no-cache -r ${PENNAIR_MONOREPO_PATH}/pyproject.toml
 
+#### install PX4 dependencies (include gz, but no nuttx hardware compiler)
+bash ${PENNAIR_PX4_PATH}/Tools/setup/ubuntu.sh --no-nuttx  # RUNS_IN_DOCKER=true set in Dockerfile
+
+#### clean up any apt package caches
+apt-get clean
+rm -rf /var/lib/apt/lists/*
