afc22c99b9ba351418e7fd4208686ab4d136e9aa
yuzhiliu8 <yuzhiliu8@gmail.com>
2026-08-08 16:18:58 -0400

    run testing methods

--- gcs/src/App.tsx
+++ gcs/src/App.tsx
@@ -1,14 +1,32 @@
 import { useState } from "react";
 import { invoke } from "@tauri-apps/api/core";
+import FoxgloveClient from "./foxglove-ws/foxglove-client";
+import { IWebSocket } from "./foxglove-ws/types";
 import "./App.css";
+
+
 
 function App() {
   const [greetMsg, setGreetMsg] = useState("");
   const [name, setName] = useState("");
 
-  async function greet() {
+
+  async function connect() {
+    const ws = new WebSocket("ws://localhost:8765", [FoxgloveClient.SUPPORTED_SUBPROTOCOL]);
+    const client = new FoxgloveClient({ ws: ws as IWebSocket });
+
+    client.on("open", () => {
+      console.log("foxglove connected");
+    });
+    client.on("advertise", (newChannels) => {
+      for (const channel of newChannels) {
+        console.log(JSON.stringify(channel, null, 2));
+      }
+    });
+
+    // client.subscribe()
     // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    setGreetMsg(await invoke("greet", { name }));
+    // setGreetMsg(await invoke("greet", { name }));
   }
 
   return (
@@ -19,15 +37,18 @@ function App() {
         className="row"
         onSubmit={(e) => {
           e.preventDefault();
-          greet();
+          connect();
         }}
       >
         <input
           id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
+          onChange={(e) => {
+            setName(e.currentTarget.value);
+            console.log("PRESSED");
+          }}
           placeholder="Enter a name..."
         />
-        <button type="submit">Greet</button>
+        <button type="submit">Connect</button>
       </form>
       <p>{greetMsg}</p>
     </main>
