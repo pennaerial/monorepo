2316f199c1e748d17a7d0acc3d03bb356fe464f9
yuzhiliu8 <yuzhiliu8@gmail.com>
2026-08-07 23:24:29 -0400

    jazzy std_msgs generation

---
+++ gcs/src/ros_interfaces.ts
@@ -0,0 +1,135 @@
+/* eslint-disable */
+// These files were generated using "ros-typescript-generator"
+export interface RosStdMsgsBool {
+  data: boolean;
+}
+
+export interface RosStdMsgsByte {
+  data: number;
+}
+
+export interface RosStdMsgsByteMultiArray {
+  layout: RosStdMsgsMultiArrayLayout;
+  data: number[];
+}
+
+export interface RosStdMsgsChar {
+  data: number;
+}
+
+export interface RosStdMsgsColorRgba {
+  r: number;
+  g: number;
+  b: number;
+  a: number;
+}
+
+export interface RosStdMsgsFloat32 {
+  data: number;
+}
+
+export interface RosStdMsgsFloat32MultiArray {
+  layout: RosStdMsgsMultiArrayLayout;
+  data: number[];
+}
+
+export interface RosStdMsgsFloat64 {
+  data: number;
+}
+
+export interface RosStdMsgsFloat64MultiArray {
+  layout: RosStdMsgsMultiArrayLayout;
+  data: number[];
+}
+
+export interface RosStdMsgsHeader {
+  stamp: { sec: number, nanosec: number };
+  frame_id: string;
+}
+
+export interface RosStdMsgsInt16 {
+  data: number;
+}
+
+export interface RosStdMsgsInt16MultiArray {
+  layout: RosStdMsgsMultiArrayLayout;
+  data: number[];
+}
+
+export interface RosStdMsgsInt32 {
+  data: number;
+}
+
+export interface RosStdMsgsInt32MultiArray {
+  layout: RosStdMsgsMultiArrayLayout;
+  data: number[];
+}
+
+export interface RosStdMsgsInt64 {
+  data: number;
+}
+
+export interface RosStdMsgsInt64MultiArray {
+  layout: RosStdMsgsMultiArrayLayout;
+  data: number[];
+}
+
+export interface RosStdMsgsInt8 {
+  data: number;
+}
+
+export interface RosStdMsgsInt8MultiArray {
+  layout: RosStdMsgsMultiArrayLayout;
+  data: number[];
+}
+
+export interface RosStdMsgsMultiArrayDimension {
+  label: string;
+  size: number;
+  stride: number;
+}
+
+export interface RosStdMsgsMultiArrayLayout {
+  dim: RosStdMsgsMultiArrayDimension[];
+  data_offset: number;
+}
+
+export interface RosStdMsgsString {
+  data: string;
+}
+
+export interface RosStdMsgsUInt16 {
+  data: number;
+}
+
+export interface RosStdMsgsUInt16MultiArray {
+  layout: RosStdMsgsMultiArrayLayout;
+  data: number[];
+}
+
+export interface RosStdMsgsUInt32 {
+  data: number;
+}
+
+export interface RosStdMsgsUInt32MultiArray {
+  layout: RosStdMsgsMultiArrayLayout;
+  data: number[];
+}
+
+export interface RosStdMsgsUInt64 {
+  data: number;
+}
+
+export interface RosStdMsgsUInt64MultiArray {
+  layout: RosStdMsgsMultiArrayLayout;
+  data: number[];
+}
+
+export interface RosStdMsgsUInt8 {
+  data: number;
+}
+
+export interface RosStdMsgsUInt8MultiArray {
+  layout: RosStdMsgsMultiArrayLayout;
+  data: number[];
+}
