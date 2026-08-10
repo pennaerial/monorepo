4f3a1f197592d4953c024885f5e59233b7a50568
yuzhiliu8 <yuzhiliu8@gmail.com>
2026-08-09 21:02:49 -0400

    add pennair command and greeting verb

---
+++ controls/sae_2025_ws/src/pennair_cli/pennair_cli/command/pennair.py
@@ -0,0 +1,33 @@
+from ros2cli.command import add_subparsers_on_demand
+from ros2cli.command import CommandExtension
+
+
+
+
+class PennairCommand(CommandExtension):
+    """Various pennair related sub-commands""" # this docstring becomes argparser description in --help
+
+
+    def add_arguments(self, parser, cli_name, *, argv=None):
+        self._subparser = parser # argparser instance for 'pennair' command
+
+        # get pennair.verb extensions and let them add their arguments
+        add_subparsers_on_demand(
+            self._subparser,
+            cli_name,
+            '_verb',
+            'pennair.verb',
+            required=False,
+            argv=argv,
+        )
+
+    def main(self, *, parser, args):
+        if not hasattr(args, '_verb'):
+            # in case no verb was passed
+            self._subparser.print_help()
+            return 0
+
+        extension = getattr(args, '_verb')
+
+        # call the verb's main method
+        return extension.main(args=args)
---
+++ controls/sae_2025_ws/src/pennair_cli/pennair_cli/verb/greeting.py
@@ -0,0 +1,51 @@
+from ros2cli.verb import VerbExtension
+
+
+def link(text: str, url: str) -> str:
+    return f"\033]8;;{url}\033\\{text}\033]8;;\033\\"
+
+
+GREETING_MESSAGE = fr"""
+
+⠀⠀⠀⠀⠀⠀⢀⣠⠴⠒⠒⠒⠒⠒⠶⠦⠤⠴⠒⠚⠉⣰⠟⠁⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⢀⡞⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠒⡿⠃⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⢠⡿⣤⣄⠀⠀⠀⠀⠀⢀⣤⣄⠀⠀⠀⣰⠞⠁⠀⠀⠀⢠⣤⠀⠀⠀⠀
+⠀⠀⢠⡟⠸⣿⡿⢀⠤⢄⠀⠐⣷⣿⣿⡷⠀⠀⢻⠀⠀⠀⢀⡴⠋⠘⡇⠀⠀⠀
+⠀⠀⢸⠈⠲⡬⢠⡏⠀⢀⡷⠀⠨⣭⠭⠖⣇⠀⣾⠀⠀⡴⠋⠀⠀⠀⣧⠀⠀⠀
+⠀⠀⢸⣷⠞⠁⠀⠳⠖⠋⠀⠀⠀⠙⠶⠶⠃⡼⢿⣠⠎⠀⠀⠀⠀⠀⣿⠀⠀⠀
+⠀⠀⢸⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡾⠃⠀⠀⠀⠀⠀⠀⡟⠀⠀⠀
+⠀⠀⡼⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⡄⠀⠀⠀⠀⠀⢠⠃⠀⠀⠄
+⢀⣰⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠐⠉⠉⠉⠉⠲⢄⡀⢀⣠⡷⠀⠀⢠
+⢸⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠓⠧⠤⢾⢀⠏
+⢸⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢨⠏⠀
+⢸⠀⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⡏⠀⠀
+⠈⢷⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠢⡀⠀⠀⠀⠀⠀⠀⢀⣤⡏⠀⠀⠀
+⠀⠈⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠑⠲⠶⠶⠖⠊⠁⣴⠇⠀⠀⠀
+⠀⠀⠘⢧⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣾⠁⠀⠀⠀⠀
+⠀⠀⠀⠈⠻⢦⣀⠀⠀⠀⠀⠀⠀⣠⣎⡀⡀⠀⠀⣀⣰⢶⠶⠚⠁⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⣰⠛⠉⡙⠛⢛⣷⠖⠒⢖⣾⠟⢛⠛⠺⣿⣏⠁⠀⠀⠀⠀⠀⠀⠀⠀
+______                 ___  _______
+| ___ \               / _ \(_) ___ \
+| |_/ /__ _ __  _ __ / /_\ \_| |_/ /
+|  __/ _ \ '_ \| '_ \|  _  | |    /
+| | |  __/ | | | | | | | | | | |\ \
+\_|  \___|_| |_|_| |_\_| |_/_\_| \_|
+
+Greetings from the PennAiR Software Team!
+
+{link("GitHub: https://github.com/pennaerial/monorepo", "https://github.com/pennaerial/monorepo")}
+{link("Docs: https://pennaerial.github.io/monorepo", "https://pennaerial.github.io/monorepo")}
+"""
+
+
+
+
+
+
+
+
+class GreetingVerb(VerbExtension):
+    """Prints out a greeting from the PennAiR Software Team."""
+
+    def main(self, *, args):
+        print(GREETING_MESSAGE)
