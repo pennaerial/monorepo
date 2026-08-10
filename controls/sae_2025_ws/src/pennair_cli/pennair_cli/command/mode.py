60004a43391153a790d89b10c2d2d8c1f90708c7
yuzhiliu8 <yuzhiliu8@gmail.com>
2026-08-10 15:04:22 -0400

    reformat

--- controls/sae_2025_ws/src/pennair_cli/pennair_cli/command/mode.py
+++ controls/sae_2025_ws/src/pennair_cli/pennair_cli/command/mode.py
@@ -1,13 +1,14 @@
-from ros2cli.verb import VerbExtension
+from pennair_cli.extension import CommandExtension
 from vehicle_common.mode_loader import ModeRegistry
 from argparse import ArgumentParser
 
 
-class ModeVerb(VerbExtension):
+# ModeRegistry causes a bunch of imports, which makes the cli pretty slow. Either optimize the import speed (opencv could be the culprit)
+# or switch to reading static json file
+class ModeCommand(CommandExtension):
     """Prints out general information about all registered modes"""
 
     def add_arguments(self, parser: ArgumentParser, cli_name: str):
-        self.registry = ModeRegistry.get()
         self.parser = parser
 
         parser.add_argument(
@@ -19,12 +20,12 @@ class ModeVerb(VerbExtension):
         parser.add_argument(
             "mode_ids",
             nargs="*",
-            choices=self.registry.modes.keys(),
             help="Prints out information about all modes with the provided ids",
         )
 
-
     def main(self, *, args):
+        print("mode command")
+        self.registry = ModeRegistry.get()
         if not args.all and not args.mode_ids:
             self.parser.print_help()
             return
@@ -40,5 +41,3 @@ class ModeVerb(VerbExtension):
                 print(f"{id} is not present in the ModeRegistry")
                 continue
             print(registered_mode, end="\n\n")
-
-
