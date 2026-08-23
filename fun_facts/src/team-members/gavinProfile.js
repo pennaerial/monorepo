4834b2aea7d30661286edd79eeff26ef86375087
yuzhiliu8 <yuzhiliu8@gmail.com>
2026-08-22 20:40:02 -0400

    remove fun facts

--- fun_facts/src/team-members/gavinProfile.js
+++
@@ -1,7 +0,0 @@
-export const profile = {
-  name: "Gavin Rauh",
-  class: { year: 2028, program: "Mechanical Engineering" },
-  location: { country: "United States", state: "New York" },
-  hobbies: ["Golf", "Philosophy", "Starting Businesses"],
-  funFacts: ["Favorite football team is the Buffalo Bills."],
-};
