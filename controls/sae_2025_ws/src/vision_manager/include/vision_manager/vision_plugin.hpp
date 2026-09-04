81db520c9c9fb2e7d5bfa93687ed7404e5bd378f
Yuzhi Liu <137526570+yuzhiliu8@users.noreply.github.com>
2026-09-04 16:48:50 -0400

    Add VisionPlugin Class (#431)
    * initial package commit
    
    * rm vision_plugins
    
    * change cfmt shortcut to cppfmt
    
    * create VisionManager and VisionPlugin header files
    
    * create main.cpp entrypoint
    
    * rename class to VisionManager and namespace it to pennair_vision

---
+++ controls/sae_2025_ws/src/vision_manager/include/vision_manager/vision_plugin.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <rclcpp/node.hpp>
+#include <rclcpp/rclcpp.hpp>
+#include "sensor_msgs/msg/image.hpp"
+
+namespace pennair_vision
+{
+
+class VisionPlugin
+{
+public:
+  virtual ~VisionPlugin() = default;
+
+  /// Initialize the plugin by passing in ROS node
+  virtual void initialize(rclcpp::Node::SharedPtr node) = 0;
+
+  /// Run the VisionPlugin impl and publish the results
+  virtual void process(sensor_msgs::msg::Image::ConstSharedPtr image) = 0;
+
+
+private:
+  /// node ptr for params and creating publishers
+  rclcpp::Node::SharedPtr node_;
+
+};
+
+
+} // pennair_vision
