3cd67c9085b1ad7741dd63b178d06ce2c8a19315
yuzhiliu8 <yuzhiliu8@gmail.com>
2026-09-16 16:24:01 -0400

    create foxglove_client with connect function and dummy background task

---
+++ experimental/gcs/src/ros/foxglove_client.rs
@@ -0,0 +1,80 @@
+use crate::ros::server_types::ServerMessage;
+use futures_util::{
+    stream::{SplitSink, SplitStream},
+    StreamExt,
+};
+use std::time::Duration;
+use tokio::time::sleep;
+use tokio::{net::TcpStream, sync::broadcast};
+use tokio_tungstenite::{
+    connect_async,
+    tungstenite::{client::IntoClientRequest, http::HeaderValue, Error, Message},
+    MaybeTlsStream, WebSocketStream,
+};
+
+pub type ServerMessageBroadcaster = broadcast::Sender<ServerMessage>;
+pub type ServerMessageReceiver = broadcast::Receiver<ServerMessage>;
+
+type Socket = WebSocketStream<MaybeTlsStream<TcpStream>>;
+type SocketReader = SplitStream<Socket>;
+type SocketWriter = SplitSink<Socket, Message>;
+
+const FOXGLOVE_SUBPROTOCOL: &str = "foxglove.sdk.v1";
+
+// Invariant: if connected, then all resources are valid
+pub struct FoxgloveClient {
+    connected: bool,
+    socket_writer: Option<SocketWriter>,
+    broadcaster: ServerMessageBroadcaster,
+}
+
+impl FoxgloveClient {
+    pub fn new() -> FoxgloveClient {
+        let (broadcaster, _) = broadcast::channel(100); // 100 max buffer
+        FoxgloveClient {
+            connected: false,
+            socket_writer: None,
+            broadcaster,
+        }
+    }
+
+    pub async fn connect(&mut self, url: &str) -> Result<(), Error> {
+        let mut request = url.into_client_request()?;
+
+        request.headers_mut().insert(
+            "Sec-WebSocket-Protocol",
+            HeaderValue::from_static(FOXGLOVE_SUBPROTOCOL),
+        );
+
+        let (socket, response) = connect_async(request).await?;
+        let protocol = response
+            .headers()
+            .get("Sec-WebSocket-Protocol")
+            .and_then(|value| value.to_str().ok())
+            .unwrap_or("None");
+
+        println!("Connection Success with subprotocol: {}", protocol);
+        // self.socket = Some(socket);
+        let (socket_writer, socket_reader) = socket.split();
+        self.socket_writer = Some(socket_writer);
+
+        // start the message handling loop
+        let broadcaster_clone = self.broadcaster.clone();
+        tokio::spawn(async move {
+            FoxgloveClient::on_message_loop(broadcaster_clone, socket_reader).await;
+        });
+        Ok(())
+    }
+
+    // keep as associated function w/o self bc we move all necessary resources into it
+    async fn on_message_loop(broadcaster: ServerMessageBroadcaster, socket_reader: SocketReader) {
+        loop {
+            println!("Dummy message!");
+            sleep(Duration::from_secs(1)).await;
+        }
+    }
+
+    pub fn listen_to_events(&mut self) -> ServerMessageReceiver {
+        self.broadcaster.subscribe()
+    }
+}
