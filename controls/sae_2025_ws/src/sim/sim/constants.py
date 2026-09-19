ef5c9d613421a4b8ed80608231cba8c51db0b8b0
Yuzhi Liu <137526570+yuzhiliu8@users.noreply.github.com>
2026-09-19 02:44:22 -0400

    Remove old sim.launch.py and unused sim code (#518)
    * remove old launch files
    
    * unused code after removing sim.launch.py
    
    * reformat
    
    * ruff check fix

--- controls/sae_2025_ws/src/sim/sim/constants.py
+++ controls/sae_2025_ws/src/sim/sim/constants.py
@@ -3,35 +3,9 @@
 Constants and enums for the sim package.
 """
 
-from enum import IntEnum
-
-
-class Competition(IntEnum):
-    """Competition type enumeration."""
-
-    IN_HOUSE = 0
-    IARC = 1
-    CUSTOM = 2
-    SAE = 3
-    IN_HOUSE_2026 = 4
-
-
-# Competition name mapping (values are the directory under simulations/ and the world .sdf stem)
-COMPETITION_NAMES = {
-    Competition.IN_HOUSE: "in_house",
-    Competition.IARC: "iarc",
-    Competition.CUSTOM: "custom",
-    Competition.SAE: "sae",
-    Competition.IN_HOUSE_2026: "in_house_2026",
-}
-
 # Platform name mapping
 PLATFORM_NAMES = {
     "win": "x86",
     "linux": "arm",
     "darwin": "arm",  # Mac
 }
-
-# Default values
-DEFAULT_COMPETITION = Competition.IN_HOUSE
-DEFAULT_USE_SCORING = False
