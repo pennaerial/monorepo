45d4cf3e98ac444e3a89dd37e6d9205c72e0c5bd
agent-hah <ashmithandoo@outlook.com>
2026-09-20 22:40:56 +0000

    refactored to use type aliases and a struct for vision manager

--- controls/sae_2025_ws/src/pennair_vision/include/pennair_vision/vision_manager.hpp
+++ controls/sae_2025_ws/src/pennair_vision/include/pennair_vision/vision_manager.hpp
@@ -7,6 +7,13 @@
 
 #include "pennair_vision/vision_plugin.hpp"
 
+struct VisionPluginStruct {
+  // add more relevant fields when necessary
+  pluginlib::UniquePtr<pennair_vision::VisionPlugin> plugin_instance_;
+};
+
+using VisionPluginMap = std::map<std::string, VisionPluginStruct>;
+
 namespace pennair_vision
 {
 
@@ -25,7 +32,7 @@ private:
   /// ClassLoader for dynamically creating VisionPlugins
   pluginlib::ClassLoader<VisionPlugin> plugin_loader_;
   /// VisionPlugin instance; key is the plugin name, value is the plugin instance
-  std::map<std::string, pluginlib::UniquePtr<VisionPlugin>> plugin_map_;
+  VisionPluginMap plugin_map_;
 };
 
 }  // namespace pennair_vision
