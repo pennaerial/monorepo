ac333ad3797b7aca4b02319734e43aa8118f671b
Kelly <kemizhang08@gmail.com>
2026-09-19 20:20:11 -0400

    Replace UAV debug parameter with logger debug (#535)
    * Replace debug parameter with logger debug
    
    * Fix formatting

--- controls/sae_2025_ws/src/uav/uav/vehicles/UAV.py
+++ controls/sae_2025_ws/src/uav/uav/vehicles/UAV.py
@@ -47,7 +47,6 @@ class UAV(Vehicle):
     def __init__(
         self,
         node: Node,
-        DEBUG=False,
         camera_offsets=[0, 0, 0],
         vehicle_name: str = "uav",
     ):
@@ -61,8 +60,6 @@ class UAV(Vehicle):
             camera_service_name="camera_data",
         )
         self.node = node
-        self.DEBUG = DEBUG
-        self.node.get_logger().info(f"Initializing UAV with DEBUG={DEBUG}")
 
         # Initialize necessary parameters to handle PX4 flight failures
         self.flight_check = False
@@ -517,10 +514,9 @@ class UAV(Vehicle):
         self.system_id = msg.system_id
         self.component_id = msg.component_id
         self.failsafe = self.failsafe_px4 or self.failsafe_trigger
-        if self.DEBUG:
-            self.node.get_logger().info(
-                f"Nav State: {self.nav_state}, Arm State: {self.arm_state}, Failsafe: {self.failsafe_px4}, Flight Check: {self.flight_check}"
-            )
+        self.node.get_logger().debug(
+            f"Nav State: {self.nav_state}, Arm State: {self.arm_state}, Failsafe: {self.failsafe_px4}, Flight Check: {self.flight_check}"
+        )
 
     def _attitude_callback(self, msg: VehicleAttitude):
         self.vehicle_attitude = msg
