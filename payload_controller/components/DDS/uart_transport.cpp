1bd23baa31f515356ba2cd676d7d3ce4282458eb
yuzhiliu8 <yuzhiliu8@gmail.com>
2026-09-24 19:36:37 -0400

    create stub implementations for a custom UartTransport

---
+++ payload_controller/components/DDS/uart_transport.cpp
@@ -0,0 +1,25 @@
+#include "uart_transport.hpp"
+
+bool uart_open(uxrCustomTransport* transport)
+{
+  // TODO
+  return true;
+}
+
+size_t uart_write(uxrCustomTransport* transport, const uint8_t* buffer, size_t length, uint8_t* error_code)
+{
+  // TODO
+  return 0;
+}
+
+size_t uart_read( uxrCustomTransport* transport, uint8_t* buffer, size_t length, int timeout, uint8_t* error_code)
+{
+  // TODO
+  return 0;
+}
+
+bool uart_close(uxrCustomTransport* transport)
+{
+  // TODO
+  return true;
+}
