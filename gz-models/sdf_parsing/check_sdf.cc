e8a88583d3f99ab494fd3219ea68366f30431b7c
Gavin <gk2008wong@gmail.com>
2026-09-26 00:01:07 -0400

    update mergebuild workflow and format cpp

--- gz-models/sdf_parsing/check_sdf.cc
+++ gz-models/sdf_parsing/check_sdf.cc
@@ -1,14 +1,11 @@
 #include <iostream>
-
 #include <sdf/sdf.hh>
 
 int main(int argc, const char* argv[])
 {
   // check arguments
-  if (argc < 2)
-  {
-    std::cerr << "Usage: " << argv[0]
-              << " <sdf-path>" << std::endl;
+  if (argc < 2) {
+    std::cerr << "Usage: " << argv[0] << " <sdf-path>" << std::endl;
     return -1;
   }
   const std::string sdfPath(argv[1]);
@@ -16,16 +13,14 @@ int main(int argc, const char* argv[])
   // load and check sdf file
   sdf::SDFPtr sdfElement(new sdf::SDF());
   sdf::init(sdfElement);
-  if (!sdf::readFile(sdfPath, sdfElement))
-  {
+  if (!sdf::readFile(sdfPath, sdfElement)) {
     std::cerr << sdfPath << " is not a valid SDF file!" << std::endl;
     return -2;
   }
 
   // start parsing model
   const sdf::ElementPtr rootElement = sdfElement->Root();
-  if (!rootElement->HasElement("model"))
-  {
+  if (!rootElement->HasElement("model")) {
     std::cerr << sdfPath << " is not a model SDF file!" << std::endl;
     return 0;
   }
@@ -35,21 +30,17 @@ int main(int argc, const char* argv[])
 
   // parse model links
   sdf::ElementPtr linkElement = modelElement->GetElement("link");
-  while (linkElement)
-  {
+  while (linkElement) {
     const std::string linkName = linkElement->Get<std::string>("name");
-    std::cout << "Found " << linkName << " link in "
-              << modelName << " model!" << std::endl;
+    std::cout << "Found " << linkName << " link in " << modelName << " model!" << std::endl;
     linkElement = linkElement->GetNextElement("link");
   }
 
   // parse model joints
   sdf::ElementPtr jointElement = modelElement->GetElement("joint");
-  while (jointElement)
-  {
+  while (jointElement) {
     const std::string jointName = jointElement->Get<std::string>("name");
-    std::cout << "Found " << jointName << " joint in "
-              << modelName << " model!" << std::endl;
+    std::cout << "Found " << jointName << " joint in " << modelName << " model!" << std::endl;
 
     const sdf::ElementPtr parentElement = jointElement->GetElement("parent");
     const std::string parentLinkName = parentElement->Get<std::string>();
@@ -57,8 +48,8 @@ int main(int argc, const char* argv[])
     const sdf::ElementPtr childElement = jointElement->GetElement("child");
     const std::string childLinkName = childElement->Get<std::string>();
 
-    std::cout << "Joint " << jointName << " connects " << parentLinkName
-              << " link to " << childLinkName << " link" << std::endl;
+    std::cout << "Joint " << jointName << " connects " << parentLinkName << " link to " << childLinkName << "link"
+              << std::endl;
 
     jointElement = jointElement->GetNextElement("joint");
   }
